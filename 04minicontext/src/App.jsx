8bd3834f5b98e3cfa36b51a802a6130cc5bb9055
chetan <cs5517767@gmail.com>
2024-10-24 00:53:25 +0530

    contextApi part-1

---
+++ 04minicontext/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Login from "./components/login"
+import Profile from "./components/profile"
+import UserContextProvider from "./context/UserContextProvider"
+
+
+
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>Context API Concept in React</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
